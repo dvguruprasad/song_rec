4ed1b2674f75acb028ec868b1ed06bb0fe3f3903
Guruprasad <dv.guruprasad@gmail.com>
2012-06-05 00:15:05 +0530

    adding counters to track time elapsed in generating item similarities

--- src/com/songrec/mappers/ItemSimilarityMapper.java
+++ src/com/songrec/mappers/ItemSimilarityMapper.java
@@ -1,5 +1,6 @@
 package com.songrec.mappers;
 
+import com.songrec.Counters;
 import com.songrec.dto.PlayCountPair;
 import com.songrec.dto.SongPair;
 import com.songrec.dto.SongPlayCountPair;
@@ -12,6 +13,7 @@ import java.io.IOException;
 public class ItemSimilarityMapper extends Mapper<Text, SongPlayCountPairs, SongPair, PlayCountPair> {
     @Override
     protected void map(Text userId, SongPlayCountPairs songPlayCountpairs, Context context) throws IOException, InterruptedException {
+        long then = System.nanoTime();
         long size = songPlayCountpairs.size();
         for(int i = 0; i < size; i++){
             for(int j = i+1; j< size; j++){
@@ -22,5 +24,6 @@ public class ItemSimilarityMapper extends Mapper<Text, SongPlayCountPairs, SongP
                 context.write(key, value);
             }
         }
+        context.getCounter(Counters.TIME_SPENT_IN_COMPUTAION_OF_SIMILARITIES_MAPPER).increment(System.nanoTime() - then);
     }
 }
--- src/com/songrec/reducers/ItemSimilarityReducer.java
+++ src/com/songrec/reducers/ItemSimilarityReducer.java
@@ -1,8 +1,8 @@
 package com.songrec.reducers;
 
+import com.songrec.algorithms.PearsonCorrelationSimilarity;
 import com.songrec.dto.PlayCountPair;
 import com.songrec.dto.SongPair;
-import com.songrec.algorithms.PearsonCorrelationSimilarity;
 import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.mapreduce.Reducer;
 
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 public class ItemSimilarityReducer extends Reducer<SongPair, PlayCountPair, SongPair, DoubleWritable> {
     @Override
     protected void reduce(SongPair songPair, Iterable<PlayCountPair> playCountVectors, Context context) throws IOException, InterruptedException {
+        long then = System.nanoTime();
         ArrayList<Float> firstItemVector = new ArrayList<Float>();
         ArrayList<Float> secondItemVector = new ArrayList<Float>();
         for(PlayCountPair pair : playCountVectors){
@@ -24,5 +25,6 @@ public class ItemSimilarityReducer extends Reducer<SongPair, PlayCountPair, Song
 
         double similarityScore = PearsonCorrelationSimilarity.score(firstItemVector, secondItemVector);
         context.write(songPair, new DoubleWritable(similarityScore));
+        context.getCounter(com.songrec.Counters.TIME_SPENT_IN_COMPUTAION_OF_SIMILARITIES_REDUCER).increment(System.nanoTime() - then);
     }
 }
